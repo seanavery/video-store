98e933ec56bee619b7afa713e935774b20291214
seanavery <seanpollock3344@gmail.com>
2025-03-07 11:03:14 -0500

    WIP get video info helper

--- videostore/utils.h
+++ videostore/utils.h
@@ -1,7 +1,8 @@
 #ifndef VIAM_VIDEOSTORE_UTILS_H
 #define VIAM_VIDEOSTORE_UTILS_H
 #include <libavformat/avformat.h>
-int get_video_duration(int64_t *duration, const char *filename);
-#define VIDEO_STORE_DURATION_RESP_ERROR 1
-#define VIDEO_STORE_DURATION_RESP_OK 0
+int get_video_info(int64_t *duration, int *width, int *height, char *codec, const char *filename);
+#define VIDEO_STORE_VIDEO_INFO_RESP_ERROR 1
+#define VIDEO_STORE_VIDEO_INFO_RESP_OK 0
+#define VIDEO_STORE_CODEC_NAME_LEN 64
 #endif /* VIAM_VIDEOSTORE_UTILS_H */
